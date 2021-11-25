7b1f9051210617c8dc2492c4a7db7680a92c501e
Adegboye <adedayo.adegboye@ger.win.int.kn>
2021-11-25 10:30:11 +0200

    updt

--- web-client/webpack.config.prod.js
+++ web-client/webpack.config.prod.js
@@ -1,8 +1,8 @@
-const merge = require("webpack-merge");
-const globalConfig = require("./webpack.config.js");
+const { merge } = require('webpack-merge');
+const globalConfig = require('./webpack.config.js');
 
 module.exports = (env) =>
   merge(globalConfig(env), {
-    mode: "production",
-    devtool: "source-map",
+    mode: 'production',
+    devtool: 'source-map',
   });
